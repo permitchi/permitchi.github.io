75e57fb82b45d52372254aa58e16c898cf694b95
permitchi <mickavallery@gmail.com>
2026-05-29 10:54:41 +0700

    init

---
+++ portfolio-site/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white">
+      <h1 className="text-5xl font-bold p-10">
+        Portfolio
+      </h1>
+    </main>
+  );
+}
